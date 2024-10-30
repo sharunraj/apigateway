6f457d07bebc4541ec3df8fa2544fc1362c34963
sharunraj <sharunraj39@gmail.com>
2024-10-30 15:54:59 +0530

    new

---
+++ src/main/java/com/example/apigateway/filter/RouteValidator.java
@@ -0,0 +1,26 @@
+package com.example.apigateway.filter;
+
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+public class RouteValidator {
+    // list of urls to be ignored by the api-gateway filter
+    public static final List<String> openApiEndpoints = List.of(
+            "/api/auth/register",
+            "/api/auth/validate/user",
+            "/api/auth/validate/token"
+    );
+    public Predicate<ServerHttpRequest> isSecured =
+            request -> openApiEndpoints
+                    .stream()
+                    .noneMatch(uri -> request
+                            .getURI()
+                            .getPath()
+                             .contains(uri)
+                    );
+}
